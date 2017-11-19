c8db6886f195e01599f1e58c4a3a9ebe6556fd9c
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-11-19 10:02:11 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,311 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Livsugar </title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/flexslider.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<!-- Header Section -->
+<section class="tophead" role="tophead">
+  <!-- Navigation Section -->
+  <header id="header">
+    <div class="header-content clearfix"> <a class="logo" href="#"><img src="images/portfolio/logo.png" style="width: auto;height: 100px;margin-top: -40px;"></a>
+      <nav class="navbar navigation" role="navigation">
+        <ul class="primary-nav">
+          <li><a href="#header-slider">Home</a></li>
+          <li class="dropdown"><a href="#services" class="dropdown-toggle" data-toggle="dropdown">Natural</a>
+			<ul class="dropdown-menu">
+			  <li><a href="#">what is stevia</a></li>
+			  <li><a href="#">History of centuries</a></li>
+			  <li><a href="#">Safe side</a></li>
+			  <li><a href="#">Stevia Plant</a></li>
+			</ul>
+          </li>
+          <li><a href="./about_us.html">About Us</a></li>
+          <li><a href="#portfolio">Our Products</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="./faqs.html">FAQs</a></li>
+        </ul>
+      </nav>
+      <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+  </header>
+  <!-- Navigation Section -->
+</section>
+<!-- Header Section -->
+<!-- Slider Section -->
+<section id="header-slider" class="section">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active"> <img src="images/slider/banner1.jpg" alt="Chania">
+      </div>
+      <div class="item"> <img src="images/slider/diabetes.png" alt="Chania" style="width: auto;height: 740px;">
+        <div class="carousel-caption">
+        </div>
+      </div>
+    </div>
+    <!-- Controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span></a> <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span></a></div>
+</section>
+<!-- Slider Section -->
+
+<!-- Service Section -->
+<section id="about_livsugar" class="section about-liv">
+  <div class="container-fluid">
+    <div class="row">
+	  <div class="col-md-6 col-sm-6">
+		<div class="">
+		  <img src="images/portfolio/work-18.jpg" class="img-responsive" alt="">
+		</div>
+	  </div>
+	  <div class="col-md-5 col-sm-6">
+		<div class="services-content">
+			<h2>About Livsugar</h2>
+			<p>LIVSUGAR, made from Stevia, is a 100% natural, chemical-free sweetener that replaces the sugar intake from your daily diet. This revolutionary product highly aids in preventing diabetes, tooth decay and weight loss. It can be utilized in food and beverages, confectioneries, pharmaceuticals and in any food industry where sweeteners are utilized. </p>
+			<p>LIVSUGAR is a sugar substitute made from three main ingredients, stevia leaf extract, erythritol and other natural flavors. To make LIVSUGAR Natural Sweetener, we first take the sweetest part of the stevia leaf and extract its natural flavor. But stevia leaf extract is 200 times sweeter than sugar, so to balance the sweetness we add a natural sugar alcohol called erythritol. Then we add a special recipe of natural flavors to create great-tasting, zero calorie sweetness. </p>
+			<a href="about_livsugar.html" class="portfolio-link"><button type="button" class="btn btn-round-lg btn-lg read-more">Read More</button></a>
+
+		</div>
+	  </div>
+	</div>
+  </div>
+</section>
+<!-- Service Section -->
+
+<!-- Service Section -->
+<section id="services" class="section about">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-offset-3 col-md-6 col-sm-6">
+		<div class="about-us">
+			<h2>About Us</h2>
+			<p>We are a renowned manufacturer and supplier of pharmaceutical drugs which find their application in the medical sector and allied areas. The product line includes
+			tablets, capsules, injections, soft gel, creams and powders.our capabilities also lies in assisting the clients to formulate guidelines that allow them to pramote
+			their products in the market and further increasing their presence.
+			</p>
+			<a href="about_us.html" class="portfolio-link"><button type="button" class="btn btn-round-lg btn-lg read-more">Read More</button></a>
+		</div>
+      </div>
+	</div>
+  </div>
+</section>
+<!-- Service Section -->
+
+<!-- portfolio grid section -->
+<section id="portfolio" class="section portfolio">
+  <div class="container-fluid">
+  	<h2 class="portfolio-title">OUR PRODUCTS</h2>
+    <div class="row">
+      <div class="col-sm-6 portfolio-item"> <a href="#" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>Livsugar</h3>
+            <h4>Natural Sweetener</h4>
+           	<button class="btn btn-circle" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus-circle" style="font-size: 50px;color: limegreen;"></i></button>
+          </div>
+        </div>
+        <img src="images/portfolio/work-10.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="#" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>Livsugar</h3>
+            <h4>Natural Sweetener</h4>
+            <button class="btn btn-circle" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus-circle" style="font-size: 50px;color: limegreen;"></i></button>
+          </div>
+        </div>
+        <img src="images/portfolio/work-11.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="#" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>Livsugar</h3>
+            <h4>Natural Sweetener</h4>
+            <button class="btn btn-circle" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus-circle" style="font-size: 50px;color: limegreen;"></i></button>
+          </div>
+        </div>
+        <img src="images/portfolio/work-12.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="#" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>Livsugar</h3>
+            <h4>Natural Sweetener</h4>
+            <button class="btn btn-circle" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus-circle" style="font-size: 50px;color: limegreen;"></i></button>
+          </div>
+        </div>
+        <img src="images/portfolio/work-13.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="#" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>Livsugar</h3>
+            <h4>Natural Sweetener</h4>
+            <button class="btn btn-circle" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus-circle" style="font-size: 50px;color: limegreen;"></i></button>
+          </div>
+        </div>
+        <img src="images/portfolio/work-14.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="#" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>Livsugar</h3>
+            <h4>Natural Sweetener</h4>
+            <button class="btn btn-circle" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus-circle" style="font-size: 50px;color: limegreen;"></i></button>
+          </div>
+        </div>
+        <img src="images/portfolio/work-10.jpg" class="img-responsive" alt=""> </a> </div>
+    </div>
+  </div>
+</section>
+<!-- portfolio grid section -->
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Livsugar</h4>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+		  <div class="col-md-6 col-sm-6">
+			<div class="">
+			  <img src="images/portfolio/work-15.jpg" class="img-responsive" alt="">
+			</div>
+		  </div>
+		  <div class="col-md-5 col-sm-6">
+			<div class="services-content">
+				<p>LIVSUGAR, made from Stevia, is a 100% natural, chemical-free sweetener that replaces the sugar intake from your daily diet. This revolutionary product highly aids in preventing diabetes, tooth decay and weight loss. It can be utilized in food and beverages, confectioneries, pharmaceuticals and in any food industry where sweeteners are utilized. </p>
+			</div>
+		  </div>
+		</div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- Service Section -->
+<section id="services" class="section services">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6 col-sm-6">
+        <div class="services-content">
+          <img src="images/portfolio/work-18.jpg" class="img-responsive" alt="">
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-6">
+		<div class="services-content">
+			<h2>About Stevia</h2>
+			<p>The plant Stevia rebaudiana has been used for more than 1,500 years by the Guaraní peoples of South America, who called it ka'a he'ê ("sweet herb").
+The leaves have been used traditionally for hundreds of years in both Brazil and Paraguay to sweeten local teas and medicines, and as a "sweet treat".
+The genus was named for Spanish botanist and physician Petrus Jacobus Stevus (Pedro Jaime Esteve 1500–1556), a professor of botany at the University of Valencia.
+			</p>
+			<a href="work-details.html" class="portfolio-link"><button type="button" class="btn btn-primary btn-round-lg btn-lg read-more">Read More</button></a>
+		</div>
+      </div>
+	</div>
+  </div>
+</section>
+<!-- Service Section -->
+
+<!-- Testimonials section -->
+<section id="testimonials" class="section testimonials no-padding">
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="flexslider">
+        <ul class="slides">
+          <li>
+            <div class="col-md-12">
+              <blockquote>
+                <h1>"Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Phasellus non dolor nibh. Nullam elementum Aenean eu leo quam..." </h1>
+                <p>Rene Brown, Open Window production</p>
+              </blockquote>
+            </div>
+          </li>
+          <li>
+            <div class="col-md-12">
+              <blockquote>
+                <h1>"Cras dictum tellus dui, vitae sollicitudin ipsum. Phasellus non dolor nibh. Nullam elementum tellus pretium feugiat shasellus non dolor nibh. Nullam elementum tellus pretium feugiat." </h1>
+                <p>Brain Rice, Lexix Private Limited.</p>
+              </blockquote>
+            </div>
+          </li>
+          <li>
+            <div class="col-md-12">
+              <blockquote>
+                <h1>"Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur...." </h1>
+                <p>Andi Simond, Global Corporate Inc</p>
+              </blockquote>
+            </div>
+          </li>
+          <li>
+            <div class="col-md-12">
+              <blockquote>
+                <h1>"Lorem ipsum dolor sit amet, consectetur adipiscing elitPhasellus non dolor nibh. Nullam elementum tellus pretium feugiat. Cras dictum tellus dui sollcitudin." </h1>
+                <p>Kristy Gabbor, Martix Media</p>
+              </blockquote>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Testimonials section -->
+
+<!-- footer section -->
+<footer id="contact" class="footer">
+  <div class="container-fluid">
+    <div class="col-md-2 left">
+      <h4>Corporate Office:</h4>
+      <p>E-2/S-9, Punyoday Park, Near Don Bosco School, Adharwadi, Kalyan (W)- 421301, Mumbai, Maharashtra, INDIA.</p>
+    </div>
+    <div class="col-md-2 left">
+      <h4>Contact</h4>
+      <p> Call: 0251 - 3245966/ +91 - 9323466906 <br>
+        Email : <a href="mailto:livkonpharma@gmail.com"> livkonpharma@gmail.com </a></p>
+    </div>
+    <div class="col-md-2 left">
+      <h4>Social presense</h4>
+      <ul class="footer-share">
+        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+      </ul>
+    </div>
+    <div class="col-md-6 right">
+      <p>© 2015 All rights reserved. All Rights Reserved<br>
+        Made with <i class="fa fa-heart pulse"></i> by <a href="http://www.designstub.com/">Livsugar</a></p>
+    </div>
+  </div>
+</footer>
+<!-- footer section -->
+
+<!-- JS FILES -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.flexslider-min.js"></script>
+<script src="js/retina.min.js"></script>
+<script src="js/modernizr.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
